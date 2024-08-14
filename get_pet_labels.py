a6ccba607ec3a189ad63e8729ef71c11a9cfc368
Amal Syahreza <syahreza.a@gmail.com>
2024-08-14 20:04:27 +0200

    clean up get_pet_labels

--- get_pet_labels.py
+++ get_pet_labels.py
@@ -57,14 +57,9 @@ def get_pet_labels(image_dir):
     # Creates list of files in directory
     in_files = listdir(image_dir)
 
-    # Processes each of the files to create a dictionary where the key
-    # is the filename and the value is the picture label (below).
-
     # Creates empty dictionary for the results (pet labels, etc.)
     results_dic = dict()
 
-    # Processes through each file in the directory, extracting only the words
-    # of the file that contain the pet image label
     for filename in in_files:
 
         # Skips file if starts with '.' (like .DS_Store of Mac OSX) because it
@@ -74,13 +69,6 @@ def get_pet_labels(image_dir):
 
         # Creates temporary label variable to hold pet label name extracted
         pet_label = extract_breed_name(filename)
-
-        # TODO: 2a. BELOW REPLACE pass with CODE that will process each
-        #          filename in the in_files list to extract the dog breed
-        #          name from the filename. Recall that each filename can be
-        #          accessed by in_files[idx]. Be certain to place the
-        #          extracted dog breed name in the variable pet_label
-        #          that's created as an empty string ABOVE
 
         # If filename doesn't already exist in dictionary add it and it's
         # pet label - otherwise print an error message because indicates
