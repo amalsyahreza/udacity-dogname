e02223853962dace7d0910ea239c5f3d7727d177
Amal Syahreza <syahreza.a@gmail.com>
2024-08-14 17:43:03 +0200

    Run formatting for classifier

--- classifier.py
+++ classifier.py
@@ -16,6 +16,7 @@ models = {'resnet': resnet18, 'alexnet': alexnet, 'vgg': vgg16}
 with open('imagenet1000_clsid_to_human.txt') as imagenet_classes_file:
     imagenet_classes_dict = ast.literal_eval(imagenet_classes_file.read())
 
+
 def classifier(img_path, model_name):
     # load the image
     img_pil = Image.open(img_path)
@@ -50,7 +51,7 @@ def classifier(img_path, model_name):
     else:
         # apply model to input
         # wrap input in variable
-        data = Variable(img_tensor, volatile = True)
+        data = Variable(img_tensor, volatile=True)
 
     # apply model to input
     model = models[model_name]
